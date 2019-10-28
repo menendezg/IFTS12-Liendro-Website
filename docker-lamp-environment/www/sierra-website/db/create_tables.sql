2bd76e5f448676e05961189422ccdfae2aea2f5c
Gabriel Menendez <gabrielmenendez@Gabriels-Air.utopia.net>
2019-10-27 21:31:13 -0300

    Add sqlite scripts for create table.
    because we need scripts for creating databases.
    also I add the database in sqlite

---
+++ docker-lamp-environment/www/sierra-website/db/create_tables.sql
@@ -0,0 +1,34 @@
+CREATE TABLE persons (
+    person_id INTEGER PRIMARY KEY,
+    name TEXT,
+    surname TEXT,
+    dni INTEGER,
+    genre TEXT,
+    phone_number INTEGER,
+    email TEXT
+); 
+
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY,
+    password TEXT NOT NULL,
+    is_administrator INTEGER NOT NULL,
+    person_id INTEGER,
+    FOREIGN KEY (person_id)
+        REFERENCES persons(person_id)
+        ON DELETE CASCADE
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE cars (
+    car_id INTEGER PRIMARY KEY,
+    brand TEXT,
+    model TEXT,
+    color TEXT,
+    doors INTEGER,
+    status TEXT,
+    person_id INTEGER,
+    FOREIGN KEY (person_id)
+        REFERENCES persons(person_id)
+        ON DELETE CASCADE
+        ON UPDATE NO ACTION
+);
