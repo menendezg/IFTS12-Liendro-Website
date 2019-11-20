667fd22567b20fbe49b933603886bd34288d6246
Gabriel Menendez <gabrielmenendez@Gabriels-Air.utopia.net>
2019-11-19 23:19:59 -0300

    Fix db update

--- docker-lamp-environment/www/sierra-website/utils/persons.php
+++ docker-lamp-environment/www/sierra-website/utils/persons.php
@@ -7,10 +7,10 @@
     $username = $session->get('username');
     $sql = "SELECT person_id from users WHERE username = '$username';";
     $result = $db->query($sql);
-    if (count($result)>0) {
-        $row = $result->fetchArray();
-        $person_id = $row['person_id'];
-    }
+if (count($result)>0) {
+    $row = $result->fetchArray();
+    $person_id = $row['person_id'];
+}
 
     $new_name = $_POST["first_name"];
     $new_surname = $_POST["last_name"];
@@ -18,17 +18,28 @@
     $new_genre = $_POST["genre"];
     $new_phone = $_POST["phone"];
 
-    $sql2 = "UPDATE persons SET name='$new_name', surname='$new_surname', dni='$new_dni', genre='$new_genre', phone_number='$new_phone' WHERE person_id='$person_id'";
+    //$sql2 = "UPDATE persons SET name='$new_name', surname='$new_surname', dni='$new_dni', genre='$new_genre', phone_number='$new_phone' WHERE person_id='$person_id'";
+
+
+    $sql2 =<<<EOF
+            UPDATE
+                persons
+            SET
+                name='$new_name', 
+                surname='$new_surname', 
+                dni='$new_dni', 
+                genre='$new_genre', 
+                phone_number='$new_phone' 
+            WHERE person_id='$person_id'
+EOF;
 
     $result2 = $db->exec($sql2);
     
-    if (count($result)>0) {
-        $status = 1;
-    } else {
-        $status = 0;
-    }
+if (count($result)>0) {
+    $status = 1;
+} else {
+    $status = 0;
+}
 
     $url = "../profile.php?status=" . $status;
     header('Location: ' . $url);
-
-?>
